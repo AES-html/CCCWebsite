24c48e4d3768551fb4624e006e23561729adb10c
AES-html <andstark@ad.unc.edu>
2026-01-26 14:05:19 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,8 +1,9 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+
+console.log("Carolina Care Collective: App is mounting...");
 
 const rootElement = document.getElementById('root');
 if (!rootElement) {
